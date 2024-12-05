602455cfdd00848f8d2b9e5b0408d9219d46397e
AdrianMares <adrianmares.dev@gmail.com>
2024-12-04 21:52:46 -0500

    reordered files and simplified javascript

--- public/main.js
+++ public/main.js
@@ -13,14 +13,22 @@ navBtnContainer.addEventListener('click', (event) => {
 
         //Remove active class from all buttons
         document.querySelectorAll('.nav-btn').forEach(btn => btn.classList.remove('active'));
+
         //Add active class back to button that is clicked on
         event.target.classList.add('active');
+
+        //Hide content sections
+        document.querySelectorAll('main section').forEach(section => section.classList.add('hidden'));
+
+        //Display corresponding section depending on the active button
+        const targetId = event.target.dataset.target;
+        document.getElementById(targetId).classList.remove('hidden');
     }
-})
+});
 
 //Display corresponding content depending on the active button
 
-studyPageBtn.addEventListener('click', () => {
+/*studyPageBtn.addEventListener('click', () => {
     studyPageContent.classList.remove('hidden');
     tasksPageContent.classList.add('hidden');
     statsPageContent.classList.add('hidden');
@@ -36,4 +44,4 @@ statsPageBtn.addEventListener('click', () => {
     studyPageContent.classList.add('hidden');
     tasksPageContent.classList.add('hidden');
     statsPageContent.classList.remove('hidden');
-})
+})*/
